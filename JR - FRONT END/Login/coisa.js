57dbcf130f09a74e0952032d9d35d43145db6e4e
thiago-gouveia <132488273+thiago-gouveia@users.noreply.github.com>
2024-05-08 22:51:00 -0300

    Add files via upload

---
+++ JR - FRONT END/Login/coisa.js
@@ -0,0 +1,7 @@
+let entradas = document.getElementById("entradas");
+function hideEntradas(){
+    entradas.classList.add("hideEntradas");
+}
+function showEntradas(){
+    entradas.classList.add("showEntradas");
+}
